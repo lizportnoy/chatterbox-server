08758f801c007ba79ba7fce38b8ea672f1b525f3
lizportnoy <lizport10@gmail.com>
2014-11-18 11:35:53 -0800

    served html

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,15 +1,10 @@
 /* Import node's http module: */
 var http = require("http");
 var handleRequest = require('./request-handler');
-var path = require('path'); // path will let you set local paths to different templates
-var fs = require('fs');
 
 var port = 3000;
 
 var ip = "127.0.0.1";
-
-
-
 
 var server = http.createServer(handleRequest.requestHandler);
 console.log("Listening on http://" + ip + ":" + port);
--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,16 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 var url = require("url");
-var results = [{username: 'anonymous', roomname: 'lobby', objectId: 0, text: 'herro'}];
+var fs = require("fs");
+var path = require("path");
+
+var results = [{
+  username: 'anonymous',
+  roomname: 'lobby',
+  objectId: 0,
+  text: 'herro'
+}];
+
 var messageIdCounter = 0;
 
 var defaultCorsHeaders = {
@@ -26,29 +35,6 @@ var defaultCorsHeaders = {
 
 var headers = defaultCorsHeaders;
 headers['Content-Type'] = "application/json";
-
-exports.requestHandler = function(request, response) {
-
-  var uri = url.parse(request.url).path.substring(0,9);
-
-  // Messages
-  if ('/classes/' === uri) {
-    switch (request.method) {
-      case 'GET':
-        getMessage(request, response);
-        break;
-
-      case 'POST':
-        postMessage(request, response);
-        break;
-
-      default:
-        badRequest(response);
-    }
-  } else {
-    notFound(response);
-  }
-};
 
 var postMessage = function (request, response) {
   var requestBody = "";
@@ -82,4 +68,78 @@ var notFound = function(response) {
   response.end('Not Found');
 };
 
+var getFile = function(localPath, response, mimeType) {
+  fs.readFile(localPath, function(err, contents) {
+    if(!err) {
+      headers['Content-Type'] = mimeType;
+      response.writeHead(200, headers);
+      response.end(contents);
+    } else {
+      response.writeHead(500);
+      response.end();
+    }
+  });
+}
 
+var getIndex = function(request, response) {
+  // Get file extensions and validate
+  var filename = request.url || "index.html";
+  var ext = path.extname(filename);
+  var localPath = '/Users/HR10/Desktop/2014-10-chatterbox-server/client/';
+  var validExtensions = {
+    ".html" : "text/html",
+    ".js": "application/javascript",
+    ".css": "text/css",
+    ".txt": "text/plain",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".png": "image/png"
+  };
+  var isValidExt = validExtensions[ext];
+
+  if (isValidExt) {
+
+    localPath += filename;
+    fs.exists(localPath, function(exists) {
+      if(exists) {
+        console.log("Serving file: " + localPath);
+        getFile(localPath, response, ext);
+      } else {
+        console.log("File not found: " + localPath);
+        response.writeHead(404);
+        response.end();
+      }
+    });
+
+  } else {
+    console.log("Invalid file extension detected: " + ext)
+  }
+}
+
+// createServer callback
+exports.requestHandler = function(request, response) {
+
+  var uri = url.parse(request.url).path.substring(0,9);
+  // Messages
+  if ('/classes/' === uri) {
+    switch (request.method) {
+      case 'GET':
+        getMessage(request, response);
+        break;
+
+      case 'POST':
+        postMessage(request, response);
+        break;
+
+      default:
+        badRequest(response);
+    }
+  } else if ('/' === uri) {
+    getIndex(request,response);
+  } else {
+    notFound(response);
+  }
+
+
+
+};
